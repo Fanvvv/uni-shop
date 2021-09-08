0fa8732dba0a1cf478f5c2eb9925e4b42d15d4fa
Lollipop <291950304@qq.com>
2021-09-08 18:46:54 +0800

    完成分类导航的渲染

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -8,6 +8,12 @@
         </navigator>
       </swiper-item>
     </swiper>
+    <!-- 分类导航 -->
+    <view class="nav-list">
+      <view class="nav-list-item" v-for="(item, i) in navList" :key="i">
+        <image :src="item.image_src" class="nav-img"></image>
+      </view>
+    </div>
 	</view>
 </template>
 
@@ -15,11 +21,13 @@
 	export default {
 		data() {
 			return {
-				swiperList: []
+				swiperList: [],
+        navList: []
 			};
 		},
     onLoad() {
       this.getSwiperList()
+      this.getNavList()
     },
     methods: {
       async getSwiperList() {
@@ -27,6 +35,12 @@
         // console.log(data)
         if (data.meta.status !== 200) return uni.$showMsg()
         this.swiperList = data.message
+      },
+      async getNavList() {
+        const { data } = await uni.$http.get('/api/public/v1/home/catitems')
+        // console.log(data)
+        if (data.meta.status !== 200) return uni.$showMsg()
+        this.navList = data.message
       }
     }
 	}
@@ -38,6 +52,15 @@ swiper {
   .swiper-item, image {
     width: 100%;
     height: 100%;
+  }
+}
+.nav-list {
+  display: flex;
+  justify-content: space-around;
+  margin: 15px 0;
+  .nav-img {
+    width: 128rpx;
+    height: 140rpx;
   }
 }
 </style>

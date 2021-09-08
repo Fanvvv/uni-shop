a4940943c914a9f5cb985c3f1559694701e4eb49
Lollipop <291950304@qq.com>
2021-09-08 16:04:40 +0800

    配置网络请求

--- main.js
+++ main.js
@@ -2,7 +2,25 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import { $http } from '@escook/request-miniprogram'
 
+// 将请求方法挂载到 uni 上
+uni.$http = $http
+
+// 请求地址
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求拦截器
+$http.beforeRequest = () => {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+
+// 响应拦截器
+$http.afterRequest = () => {
+  url.hideLoading()
+}
 Vue.config.productionTip = false
 
 App.mpType = 'app'

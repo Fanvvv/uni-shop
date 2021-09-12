a861ffae0d92782c67d6176dde713a3a15602d5a
Lollipop <291950304@qq.com>
2021-09-12 21:13:08 +0800

    渲染搜索建议列表

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -4,6 +4,15 @@
       <!-- 使用 uni-ui 提供的搜索组件 -->
       <uni-search-bar @input="input" :radius="100" cancelButton="none"></uni-search-bar>
     </view>
+    <!-- 搜索建议列表页 -->
+    <view class="sugg-list">
+      <view class="sugg-item" v-for="(item, i) in searchResultList.goods" :key="i" @click="toDetail(item.goods_id)">
+        <view class="goods-name">
+          {{ item.goods_name }}
+        </view>
+        <uni-icons type="arrowright" size="16"></uni-icons>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -41,6 +50,12 @@
         // console.log(data)
         if (data.meta.status !== 200) return uni.$showMsg()
         this.searchResultList = data.message
+      },
+      // 根据商品id跳转到商品详情页
+      toDetail(id) {
+        uni.navigateTo({
+          url: '/subpkg/goods-detail/goods-detail?goods_id=' + id
+        })
       }
     }
   }
@@ -51,5 +66,25 @@
   position: sticky;
   top: 0;
   z-index: 999;
+}
+.sugg-list {
+  padding: 0 5px;
+  .sugg-item {
+    font-size: 12px;
+    padding: 13px 0;
+    border-bottom: 1px solid #efefef;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .goods-name {
+      // 文字不允许换行（单行文本）
+      white-space: nowrap;
+      // 溢出部分隐藏
+      overflow: hidden;
+      // 文本溢出后，使用 ... 代替
+      text-overflow: ellipsis;
+      margin-right: 3px;
+    }
+  }
 }
 </style>

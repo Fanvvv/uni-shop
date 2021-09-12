cc8b255fce1bbdc6ceffbe0bb3164a56e693b520
Lollipop <291950304@qq.com>
2021-09-12 20:19:33 +0800

    渲染搜索页面的基本结构

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -1,6 +1,9 @@
 <template>
   <view>
-    search
+    <view class="search-box">
+      <!-- 使用 uni-ui 提供的搜索组件 -->
+      <uni-search-bar @input="input" :radius="100" cancelButton="none"></uni-search-bar>
+    </view>
   </view>
 </template>
 
@@ -10,10 +13,19 @@
       return {
         
       };
+    },
+    methods: {
+      input(e) {
+        console.log(e)
+      }
     }
   }
 </script>
 
 <style lang="scss">
-
+.search-box {
+  position: sticky;
+  top: 0;
+  z-index: 999;
+}
 </style>

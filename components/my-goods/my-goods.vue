50ea56631df404581e918017a4a633aeef989bb0
Lollipop <291950304@qq.com>
2021-09-13 23:07:17 +0800

    将goods-item封装为组件

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,65 @@
+<template>
+  <view>
+    <view class="goods-item">
+      <view class="goods-item-left">
+        <image :src="goods.goods_small_logo || defaultPic" mode="widthFix" class="goods-pic"></image>
+      </view>
+      <view class="goods-item-right">
+        <view class="goods-name">
+          {{ goods.goods_name }}
+        </view>
+        <view class="goods-info">
+          <view class="goods-price">
+            &yen;{{ goods.goods_price }}
+          </view>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name:"my-goods",
+    data() {
+      return {
+        // 默认的空图片
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+      };
+    },
+    props: {
+      goods: {
+        type: Object,
+        default: () => ({})
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+.goods-item {
+  display: flex;
+  padding: 10px 5px;
+  border-bottom: 1px solid #f0f0f0;
+  &-left {
+    margin-right: 5px;
+    .goods-pic {
+      width: 100px;
+      height: 100px;
+      display: block;
+    }
+  }
+  &-right {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .goods-name {
+      font-size: 13px;
+    }
+    .goods-price {
+      font-size: 16px;
+      color: #c00000;
+    }
+  }
+}
+</style>
--- subpkg/goods-list/goods-list.vue
+++ subpkg/goods-list/goods-list.vue
@@ -2,21 +2,7 @@
   <view>
     <view class="goods-list">
       <block v-for="goods in goodsList" :key="goods.goods_id">
-        <view class="goods-item">
-          <view class="goods-item-left">
-            <image :src="goods.goods_small_logo || defaultPic" mode="widthFix" class="goods-pic"></image>
-          </view>
-          <view class="goods-item-right">
-            <view class="goods-name">
-              {{ goods.goods_name }}
-            </view>
-            <view class="goods-info">
-              <view class="goods-price">
-                &yen;{{ goods.goods_price }}
-              </view>
-            </view>
-          </view>
-        </view>
+        <my-goods :goods="goods"></my-goods>
       </block>
     </view>
   </view>
@@ -40,9 +26,7 @@
         // 商品列表的数据
         goodsList: [],
         // 总数量，用来实现分页
-        total: 0,
-        // 默认的空图片
-        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+        total: 0
       };
     },
     onLoad(options) {
@@ -66,29 +50,5 @@
 </script>
 
 <style lang="scss">
-.goods-item {
-  display: flex;
-  padding: 10px 5px;
-  border-bottom: 1px solid #f0f0f0;
-  &-left {
-    margin-right: 5px;
-    .goods-pic {
-      width: 100px;
-      height: 100px;
-      display: block;
-    }
-  }
-  &-right {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    .goods-name {
-      font-size: 13px;
-    }
-    .goods-price {
-      font-size: 16px;
-      color: #c00000;
-    }
-  }
-}
+
 </style>

b54bc07b0af853f8e6e3cbebfb82b30322352b0d
Lollipop <291950304@qq.com>
2021-09-14 20:32:38 +0800

    配置vuex

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import { $http } from '@escook/request-miniprogram'
+import store from './store/index.js'
 
 // 将请求方法挂载到 uni 上
 uni.$http = $http
@@ -34,6 +35,7 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+    ...App,
+    store
 })
 app.$mount()
---
+++ store/index.js
@@ -0,0 +1,13 @@
+import Vue from "vue"
+import Vuex from 'vuex'
+
+// 使用 vuex 插件
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  // 挂载 store 模块
+  modules: {}
+})
+
+export default store
+

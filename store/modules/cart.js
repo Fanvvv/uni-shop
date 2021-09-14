927208b47fa9b4cba5703e1f7e998bf07525ba10
Lollipop <291950304@qq.com>
2021-09-14 21:54:55 +0800

    持久化存储购物车中的商品

--- store/modules/cart.js
+++ store/modules/cart.js
@@ -6,7 +6,7 @@ export default {
     // 购物车的数组，用来存储购物车中每个商品的信息对象
     // 每个商品的信息对象，都包含如下 6 个属性：
     // { goods_id, goods_name, goods_price, goods_count, goods_small_logo, goods_state }
-    cart: [],
+    cart: JSON.parse(uni.getStorageSync('cart') || '[]'),
   }),
   // 模块的 mutations 方法
   mutations: {
@@ -18,6 +18,12 @@ export default {
       } else {
         result.goods_count++
       }
+      // 调用保存到本地存储的方法
+      this.commit('cart/saveToStorage')
+    },
+    // 保存到本地存储
+    saveToStorage(state) {
+      uni.setStorageSync('cart', JSON.stringify(state.cart))
     }
   },
   // 模块的 getters 属性

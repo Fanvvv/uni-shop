a7b060fb91616bfac4476d9c435f37c5b42bb360
Lollipop <291950304@qq.com>
2021-09-14 22:14:36 +0800

    动态为 tabBar 页面设置数字徽标

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -5,12 +5,29 @@
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
 	export default {
 		data() {
 			return {
 				
 			};
-		}
+		},
+    computed: {
+      ...mapGetters('cart', ['total'])
+    },
+    onShow() {
+      // 页面显示的时候加载
+      this.setBadge()
+    },
+    methods: {
+      setBadge() {
+        // 设置购物车右上角的徽标
+        uni.setTabBarBadge({
+          index: 2,
+          text: this.total + '' // 这里是加上字符串
+        })
+      }
+    }
 	}
 </script>
 

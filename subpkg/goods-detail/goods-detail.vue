40294f2337f2386a5b307f45f0f75f4056bf5ca1
Lollipop <291950304@qq.com>
2021-09-14 13:36:40 +0800

    获取商品详情数据

--- subpkg/goods-detail/goods-detail.vue
+++ subpkg/goods-detail/goods-detail.vue
@@ -8,8 +8,22 @@
   export default {
     data() {
       return {
-        
+        // 商品详情
+        goodsInfo: {}
       };
+    },
+    onLoad(options) {
+      // 获取传入的商品 id
+      const goodsId = options.goods_id
+      this.getGoodsDetail(goodsId)
+    },
+    methods: {
+      async getGoodsDetail(goodsId) {
+        const { data } = await uni.$http.get('/api/public/v1/goods/detail', { goods_id: goodsId})
+        console.log(data)
+        if (data.meta.status !== 200) return uni.$showMsg()
+        this.goodsInfo = data.message
+      }
     }
   }
 </script>

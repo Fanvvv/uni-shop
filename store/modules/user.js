b51bd97af6a9cad4a7524e7c0ea55db973ed4765
Lollipop <291950304@qq.com>
2021-09-24 22:40:09 +0800

    将地址信息持久化存储到本地

--- store/modules/user.js
+++ store/modules/user.js
@@ -1,12 +1,19 @@
 export default {
   namespaced: true,
   state: () => ({
-    address: {}
+    // 获取本地储存的地址
+    address: JSON.parse(uni.getStorageSync('address') || '{}')
   }),
   mutations: {
     // 更新收货地址
     updateAddress(state, address) {
       state.address = address
+      // 对 address 持久化
+      this.commit('user/saveAddressToStorage')
+    },
+    // 将 address 持久化存储到本地
+    saveAddressToStorage(state) {
+      uni.setStorageSync('address', JSON.stringify(state.address))
     }
   },
   getters: {}

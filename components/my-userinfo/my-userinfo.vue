eed46b7afefb71ec2b74cee6dbfee6b4541f3b4c
Lollipop <291950304@qq.com>
2021-09-26 16:33:56 +0800

    渲染用户的头像和昵称

--- components/my-userinfo/my-userinfo.vue
+++ components/my-userinfo/my-userinfo.vue
@@ -1,20 +1,54 @@
 <template>
-  <view>
-    用户页
+  <view class="my-userinfo-container">
+    <!-- 头像昵称区域 -->
+    <view class="top-box">
+      <image :src="userInfo.avatarUrl" class="avatar"></image>
+      <view class="nickname">{{ userInfo.nickName }}</view>
+    </view>
   </view>
 </template>
 
 <script>
+  import { mapState } from 'vuex'
   export default {
     name:"my-userinfo",
     data() {
       return {
         
       };
+    },
+    computed: {
+      ...mapState('user', ['userInfo'])
     }
   }
 </script>
 
 <style lang="scss">
-
+.my-userinfo-container {
+  height: 100%;
+  // 为整个组件的结构添加浅灰色的背景
+  background-color: #f4f4f4;
+  .top-box {
+    height: 400rpx;
+    background-color: #c00000;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    .avatar {
+      display: block;
+      width: 90px;
+      height: 90px;
+      border-radius: 45px;
+      border: 2px solid white;
+      box-shadow: 0 1px 5px black;
+    }
+    .nickname {
+      color: white;
+      font-weight: bold;
+      font-size: 16px;
+      margin-top: 10px;
+    }
+  }
+}
 </style>

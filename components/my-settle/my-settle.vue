59d05fa382ac8f8348e29fe8b5928130d99d16dd
Lollipop <291950304@qq.com>
2021-09-26 17:03:27 +0800

    未登录时跳转到登录页

--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -19,7 +19,8 @@
     name:"my-settle",
     data() {
       return {
-        
+        seconds: 3, // 倒计时的秒数
+        timer: null
       };
     },
     computed: {
@@ -44,7 +45,44 @@
         // 判断是否选择了收货地址
         if (!this.fullAddress) return uni.$showMsg('请选择收货地址')
         // 判断是否登录了
-        if (!this.token) return uni.$showMsg('请先登录')
+        // if (!this.token) return uni.$showMsg('请先登录')
+        // 如果没有登录，则多少秒后跳转到登录页
+        if (!this.token) return this.delayNavigate()
+      },
+      // 提示信息
+      showTips(n) {
+        uni.showToast({
+          icon: 'none',
+          // 提示的消息
+          title: '请登录后再结算！' + n + ' 秒后自动跳转到登录页',
+          // 为页面添加透明遮罩，防止点击穿透
+          mask: true,
+          // 1.5 秒后自动消失
+          duration: 1500
+        })
+      },
+      // 延时跳转
+      delayNavigate() {
+        // 把 data 中的秒数重置成 3 秒
+        this.seconds = 3
+        this.showTips(this.seconds)
+        // 创建定时器，每隔一秒钟执行一次
+        this.timer = setInterval(() => {
+          // 先让秒数自减 1
+          this.seconds--
+          if (this.seconds <= 0) {
+            // 清除定时器
+            clearInterval(this.timer)
+            // 跳转到 my 页面
+            uni.switchTab({
+              url: '/pages/my/my'
+            })
+            // 终止后续代码的运行（当秒数为 0 时，不再展示 toast 提示消息）
+            return
+          }
+          // 再根据最新的秒数，进行消息提示
+          this.showTips(this.seconds)
+        }, 1000)
       }
     }
   }
